45687a09d1f825bd880b85c2f7849b3a0f7d33d1
DavidLee008 <597003033@qq.com>
2017-03-14 20:36:43 +0800

    third commit

--- zhihu/zhihu/items.py
+++ zhihu/zhihu/items.py
@@ -14,7 +14,7 @@ class ZhihuItem(scrapy.Item):
     pass
     urls = scrapy.Field()
     title = scrapy.Field()
-    description = scrapy.Field()
+    #description = scrapy.Field()
     answer = scrapy.Field()
     name = scrapy.Field()
     url = scrapy.Field()
